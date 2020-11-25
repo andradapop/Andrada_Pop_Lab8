1c463760896775802a5671939d85215acb4c8ce5
ANDRADA-ROBERTA POP <andrada.roberta.pop@stud.ubbcluj.ro>
2020-11-25 22:11:48 +0200

    Add project files.

---
+++ Data/Andrada_Pop_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Andrada_Pop_Lab8.Models;
+
+namespace Andrada_Pop_Lab8.Data
+{
+    public class Andrada_Pop_Lab8Context : DbContext
+    {
+        public Andrada_Pop_Lab8Context (DbContextOptions<Andrada_Pop_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Andrada_Pop_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Andrada_Pop_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Andrada_Pop_Lab8.Models.BookCategory> BookCategory { get; set; }
+    }
+}
---
+++ Pages/Publishers/Create.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Andrada_Pop_Lab8.Data;
+using Andrada_Pop_Lab8.Models;
+
+namespace Andrada_Pop_Lab8.Pages.Publishers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Andrada_Pop_Lab8.Data.Andrada_Pop_Lab8Context _context;
+
+        public CreateModel(Andrada_Pop_Lab8.Data.Andrada_Pop_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Publisher Publisher { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Publisher.Add(Publisher);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
+
+
+
+
+
